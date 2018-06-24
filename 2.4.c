65ac64b02026f22b140eaf16da2effe8271c1a55
harithadasanayaka <40230325+harithadasanayaka@users.noreply.github.com>
2018-06-24 11:06:43 +0530

    Add files via upload

---
+++ 2.4.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	int a = 3;
+	float b = 10.0;
+	float c;
+	c = a/b;
+	printf("A is %d\n",a);
+	printf("B is %f\n",b);
+	printf("Answer is %2f\n",c);
+	return 0;
+	}
+				
+
